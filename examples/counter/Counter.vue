2ba118cef3a974e04fd364fd1aa8d4dc7d910580
erfan01 <15829265101@163.com>
2019-06-26 15:49:43 +0800

    init project

---
+++ examples/counter/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  //在vue 组件中展示VUEX的状态，
+  computed: mapGetters([
+    'evenOrOdd'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ])
+}
+</script>
